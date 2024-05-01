16143701788c139c7735f7229d287193c63c5694
OhSiHyeon <osh000308@gmail.com>
2024-05-01 21:04:25 +0900

    [FEAT] MVVM 구조 셋팅

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CoordiFinder from "./views/page/CoordiFinderView";
+import CoordiFinderModel from "./views/page/CoordiFinderModel";
+import CoordiFinderViewModel from "./views/page/CoordiFinderViewModel";
 
 function App() {
+    const model = new CoordiFinderModel();
+    const viewModel = new CoordiFinderViewModel(model);
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/CoordiFinder" element={<CoordiFinder />}></Route>
+                <Route
+                    path="/CoordiFinder"
+                    element={<CoordiFinder viewModel={viewModel} />}
+                ></Route>
             </Routes>
         </BrowserRouter>
     );

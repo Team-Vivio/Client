aa73edbe3d63079e0911b32311c00400a9996b23
OhSiHyeon <osh000308@gmail.com>
2024-05-14 13:30:23 +0900

    [FEAT] 기능 1 헤더와 연결

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Main from "./views/page/main";
+import Function1 from "./views/page/bodyAnalyze";
+import Function1_ViewModel from "./views/page/boduAnalyzeViewModel";
+import Function1_Model from "./views/page/bodyAnalyzeModel";
 import Function4 from "./views/page/CoordiFinderView";
 import Function4_ViewModel from "./views/page/CoordiFinderViewModel";
 import Function4_Model from "./views/page/CoordiFinderModel";
 import Header from "./views/component/header";
 
 function App() {
+    const model_1 = new Function1_Model();
+    const viewModel_1 = new Function1_ViewModel(model_1);
+
     const model_4 = new Function4_Model();
     const viewModel_4 = new Function4_ViewModel(model_4);
 
@@ -20,6 +26,11 @@ function App() {
                     exact
                     path="/CoordiFinder"
                     element={<Function4 viewModel={viewModel_4} />}
+                />
+                <Route
+                    exact
+                    path="/FashionRecommend"
+                    element={<Function1 viewModel={viewModel_1} />}
                 />
             </Routes>
         </div>

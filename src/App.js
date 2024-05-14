85c01c8c04011f2e4e7efaad79c1ca8f51290f68
1Fe <dnjscjf0829@naver.com>
2024-05-15 00:41:56 +0900

    [fix] routes 수정
    routes, package.json 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import logo from "./logo.svg";
 
@@ -15,56 +14,51 @@ import Function4 from "./views/page/CoordiFinderView";
 import Function4_ViewModel from "./views/page/CoordiFinderViewModel";
 import Function4_Model from "./views/page/CoordiFinderModel";
 import Header from "./views/component/header";
-import { Routes, Route } from "react-router-dom";
-import Tone from "./views/page/ToneAnalyze";
-import ToneAnalyzeModel from "./views/page/ToneAnalyzeModel";
-import ToneAnalyzeViewModel from "./views/page/ToneAnalyzeViewModel";
-import ClothesFinder from "./views/page/ClothesFinder.js";
-import ClothesFinderModel from "./views/page/ClothesFinderModel.js";
-import ClothesFinderViewModel from "./views/page/ClothesFinderViewModel.js";
+import Function2 from "./views/page/ToneAnalyze";
+import Function2_Model from "./views/page/ToneAnalyzeModel";
+import Function2_ViewModel from "./views/page/ToneAnalyzeViewModel";
+import Function3 from "./views/page/ClothesFinder.js";
+import Function3_Model from "./views/page/ClothesFinderModel.js";
+import Function3_ViewModel from "./views/page/ClothesFinderViewModel.js";
 import "./App.css";
 
 function App() {
     const model_1 = new Function1_Model();
     const viewModel_1 = new Function1_ViewModel(model_1);
-    const model = new ToneAnalyzeModel();
-    const viewModel = new ToneAnalyzeViewModel(model);
+    const model_2 = new Function2_Model();
+    const viewModel_2 = new Function2_ViewModel(model_2);
+    const model_3 = new Function3_Model();
+    const viewModel_3 = new Function3_ViewModel(model_3);
     const model_4 = new Function4_Model();
     const viewModel_4 = new Function4_ViewModel(model_4);
-    const model_3 = new ClothesFinderModel();
-    const viewModel = new ClothesFinderViewModel(model_3);
+
     return (
         <div>
             <Header />
             <Routes>
                 <Route exact path="/" element={<Main />} />
-                <Route
-                    exact
-                    path="/CoordiFinder"
-                    element={<Function4 viewModel={viewModel_4} />}
-                />
                 <Route exact path="/Login" element={<Login />} />
-				<Route exact path="/Signup" element={<Signup />} />
-				<Route exact path="Mypage" element={<Mypage />} />
+                <Route exact path="/Signup" element={<Signup />} />
+                <Route exact path="Mypage" element={<Mypage />} />
                 <Route
-                    exact
                     path="/FashionRecommend"
                     element={<Function1 viewModel={viewModel_1} />}
                 />
                 <Route
                     path="/PersonalColor"
-                    element={<Tone viewModel={viewModel} />}
+                    element={<Function2 viewModel={viewModel_2} />}
                 />
                 <Route
                     path="/ClothesFinder"
-                    element={<ClothesFinder viewModel={viewModel} />}
-                ></Route>
+                    element={<Function3 viewModel={viewModel_3} />}
+                />
+                <Route
+                    path="/CoordiFinder"
+                    element={<Function4 viewModel={viewModel_4} />}
+                />
             </Routes>
         </div>
     );
-    
-    
-
 }
 
 export default App;

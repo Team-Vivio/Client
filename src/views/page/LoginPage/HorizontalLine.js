e478147efb4f274f079927b44d353099efc45f29
SangHun Kwon <jun00739@naver.com>
2024-05-20 17:47:12 +0900

    [fix] 로그인 페이지 last fix
    로고들 크기, horizon line 수정

--- src/views/page/LoginPage/HorizontalLine.js
+++ src/views/page/LoginPage/HorizontalLine.js
@@ -4,14 +4,24 @@ const HorizonLine = ({ text }) => {
 	return (
 		<div
 			style={{
-				width: "380px",
+				width: "19.792vw",
 				textAlign: "center",
 				borderBottom: "1px solid #aaa",
 				lineHeight: "0.1em",
-				margin: "10px 0 20px",
+				margin: "1.026vh 0 2.051vh",
 			}}
 		>
-			<span style={{ background: "#fff", padding: "0 10px" }}>{text}</span>
+			<span style={{ 
+				background: "#fff",
+			 	padding: "0 10px",
+				fontweight: 500,
+				fontSize: "2.564vh",
+				// lineHeight: "140%",
+				letterSpacing: "0.02em",
+				textTransform: "capitalize",
+				color: "#626262"
+
+			 }}>{text}</span>
 		</div>
 	);
 };
